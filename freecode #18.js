846fd44b3c6653f543c2ecde0377feb4d9a48ff5
Brian Grifinn <126880335+BrianGrifinn@users.noreply.github.com>
2023-11-24 13:13:50 +0200

    Add files via upload

---
+++ freecode #18.js
@@ -0,0 +1,15 @@
+/*
+Modify the existing declarations and assignments so their names use camelCase.
+
+Do not create any new variables.
+*/
+
+
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
