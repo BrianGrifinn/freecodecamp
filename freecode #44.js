f422430398203e8691fb4320b159837775fa9a0e
Brian Grifinn <126880335+BrianGrifinn@users.noreply.github.com>
2023-12-05 09:26:58 +0200

    Add files via upload

---
+++ freecode #44.js
@@ -0,0 +1,9 @@
+/*
+    Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+*/
+
+
+const someAdjective = "Slavi";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective;
