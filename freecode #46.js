dc2163ffa3673263920b3a6df990740fc603c22e
Brian Grifinn <126880335+BrianGrifinn@users.noreply.github.com>
2023-12-06 09:52:25 +0200

    Add files via upload

---
+++ freecode #46.js
@@ -0,0 +1,11 @@
+/*
+    Use bracket notation to find the first character in the lastName
+    variable and assign it to firstLetterOfLastName.
+*/
+
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+
+firstLetterOfLastName = lastName[0]
