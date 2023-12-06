ee7d3da635cdef01f2e5ab2d344cdf076772c404
Brian Grifinn <126880335+BrianGrifinn@users.noreply.github.com>
2023-12-06 09:53:07 +0200

    Add files via upload

---
+++ freecode #48.js
@@ -0,0 +1,11 @@
+/*
+    Let's try to set thirdLetterOfLastName to equal the third letter of 
+    the lastName variable using bracket notation.
+*/
+
+
+
+const lastName = "Lovelace";
+
+
+const thirdLetterOfLastName = lastName[2];
